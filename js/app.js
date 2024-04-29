8742b1543e6456531c34cd9fdc7678b46007e733
enochjeong <enochjeong@Enochui-MacBookPro.local>
2024-04-29 12:49:52 +0900

    0429

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+// 추상화된 함수 
+/**
+ * 
+ * @param {NodeList} elmts - selcected elements from HTML to add events
+ * @param {string} event - event type e.g. 'click', 'mouseenter'...
+ * @param {function} callback - callback function to be excuted when event is triggered
+ */
+
+export function eventOnElmts(elmts, event, callback) {
+    for(const elmt of elmts){
+        elmt.addEventListener(event, callback);
+    }
+}
+
